3ee2e9c8a71e610bb49f448178781bef45aacf09
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2025-03-16 19:34:26 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+total_score = 0
+
+print("(John Matthew Bautista) What is the center of the earth called?")
+print("a) Core                  b) Mantle")
+print("c) Moho                  d) Crust")
+
+answer = input("Enter your answer: ")
+
+if (answer.upper() == 'A'):
+    total_score += 1
+    print("Correct!, the answer is A.")
+else:
+    print(f"{answer} is incorrect. The correct answer is A.")
+
+# TODO(Banzali): Add second question
+
+# TODO(Managbanag): Add third question
+
+# TODO(Raymundo): Add fourth question
+
+# TODO(Espinola): Add fifth question and print total score
